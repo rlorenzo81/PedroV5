0f308e3c90af58e74071c391844e324204dfe11e
rlorenzo81 <44610558+rlorenzo81@users.noreply.github.com>
2026-02-26 13:43:30 -0500

    updates

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RedFrontV9.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RedFrontV9.java
@@ -11,6 +11,7 @@ import com.qualcomm.hardware.limelightvision.LLResult;
 import com.qualcomm.hardware.limelightvision.Limelight3A;
 import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 
 import com.qualcomm.robotcore.hardware.*;
@@ -19,6 +20,7 @@ import org.firstinspires.ftc.robotcore.external.navigation.*;
 import org.firstinspires.ftc.teamcode.pedroPathing.Constants;
 
 @Autonomous(name = "Red Front V9", group = "Examples")
+@Disabled
 public class RedFrontV9 extends OpMode {
 
     // ================= HARDWARE =================
